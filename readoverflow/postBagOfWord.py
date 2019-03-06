1297eae9e1583c9a0e6f7a7501269577be2460c4
Haibo Yan <tristan.yim@gmail.com>
2019-03-05 20:09:53 -0800

    add code for cal bow

--- readoverflow/postBagOfWord.py
+++ readoverflow/postBagOfWord.py
@@ -1,10 +1,18 @@
 import sys
+from bs4 import BeautifulSoup
 
 def main(argv):
-    with open(argv[1]) as csvf:
+    with open(argv[1], "rt") as csvf:
+        count = 0
+    # with open("10000\stackoverflow_posts.csv", "wt") as csvf:
         for line in csvf:
             cols = line.split(",")
-            print(cols)
+            print("javascipt" in cols[18].lower())
+            soup = BeautifulSoup(cols[2], 'html.parser')
+            # print(soup.get_text())
+            if count > 1000:
+                break
+            count += 1
 
 
 if __name__ == "__main__":
