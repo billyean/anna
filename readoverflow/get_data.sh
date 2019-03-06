af75204b9b0f1bd78cc18e4da4227b1f2c260276
Haibo Yan <tristan.yim@gmail.com>
2019-03-05 20:45:29 -0800

    Fix shell script

--- readoverflow/get_data.sh
+++ readoverflow/get_data.sh
@@ -2,15 +2,16 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS=./credential.json
 
-export limit=10
+export limit=1000000
 
-if [ -n $1 ]; then
+if [ "$#" -eq 1 ]; then
     export limit=$1
 fi
 
+# for table in 'stackoverflow_posts'
 for table in 'stackoverflow_posts' 'votes' 'users' 'tags' 'posts_wiki_placeholder' 'posts_tag_wiki_excerpt' 'posts_tag_wiki' \
         'posts_questions' 'posts_privilege_wiki' 'posts_orphaned_tag_wiki' 'posts_moderator_nomination' \
         'posts_answers' 'post_links' 'post_history' 'comments' 'badges'
 do
-    python3 read_stackoverflow.py ${limit} ${table}
+    python read_stackoverflow.py ${limit} ${table}
 done
